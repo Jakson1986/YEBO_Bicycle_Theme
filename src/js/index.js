ee58764c6da0e84b5a6e89c7e4fa1bab2d8319a3
Евгений Евгений <savok310586@gmail.com>
2021-10-21 22:20:57 +0300

    after lesson 19

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,13 @@
 window.$ = window.jQuery = require('jquery');
+
+$(document).ready(function () {
+    $('.header').on('click', '.header__mobile-menu, .navigation__close', function () {
+        $('.navigation').slideToggle();
+    })
+});
+
+$(window).resize(function () {
+    if ($(window).width() >= 768) {
+        $('.navigation').attr('style', '')
+    }
+})
